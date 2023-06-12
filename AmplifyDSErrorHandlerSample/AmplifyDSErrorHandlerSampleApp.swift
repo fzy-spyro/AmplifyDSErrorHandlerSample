cdcda2b0a68b222d9f2642522c889526462f26cd
Filip Zymek <fzy@spyro-soft.com>
2023-06-12 10:15:53 +0200

    implement adding/fetching todos

--- AmplifyDSErrorHandlerSample/AmplifyDSErrorHandlerSampleApp.swift
+++ AmplifyDSErrorHandlerSample/AmplifyDSErrorHandlerSampleApp.swift
@@ -1,12 +1,43 @@
-//
-
+import Amplify
+import AWSDataStorePlugin
+import AWSAPIPlugin
+import ClientRuntime
 import SwiftUI
 
 @main
 struct AmplifyDSErrorHandlerSampleApp: App {
+
+    init() {
+        do {
+            // AmplifyModels is generated in the previous step
+            let dataStorePlugin = AWSDataStorePlugin(modelRegistration: AmplifyModels(), configuration: .custom(errorHandler: dataStoreErrorHandler))
+            try Amplify.add(plugin: dataStorePlugin)
+            try Amplify.add(plugin: AWSAPIPlugin())
+
+            SDKLoggingSystem.initialize(logLevel: .debug)
+            Amplify.Logging.logLevel = .debug
+
+
+            try Amplify.configure()
+            print("Amplify configured with DataStore plugin")
+        } catch {
+            print("Failed to initialize Amplify with \(error)")
+        }
+    }
+
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+
+//MARK: - AmplifyDSErrorHandlerSampleApp
+extension AmplifyDSErrorHandlerSampleApp {
+
+    func dataStoreErrorHandler(_ amplifyError: AmplifyError) {
+        print("dataStoreErrorHandler called with: \(amplifyError)")
     }
 }
--- AmplifyDSErrorHandlerSample/ContentView.swift
+++ AmplifyDSErrorHandlerSample/ContentView.swift
@@ -1,21 +1,61 @@
 //
 
+import Amplify
 import SwiftUI
 
 struct ContentView: View {
+
+    @StateObject var viewModel = ContentViewModel()
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button("Add random todo") {
+                viewModel.generateRandomTodo()
+            }
+            List(viewModel.todos) { todo in 
+                Text(todo.name)
+            }
         }
         .padding()
+        .task {
+            await viewModel.observeTodos()
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+
+@MainActor
+final class ContentViewModel: ObservableObject {
+
+    @Published var todos = [Todo]()
+
+    private var todoSubscription: AmplifyAsyncThrowingSequence<DataStoreQuerySnapshot<Todo>>?
+
+    func observeTodos() async {
+        let subscription = Amplify.DataStore.observeQuery(for: Todo.self)
+        self.todoSubscription = subscription
+
+        do {
+            for try await querySnapshot in subscription {
+                print("Have: \(querySnapshot.items.count) todos")
+                self.todos = querySnapshot.items
+            }
+        } catch {
+            print("Error happend observing todos: \(error)")
+        }
+    }
+
+    func generateRandomTodo() {
+        Task {
+            let todo = Todo(name: "This is a todo item created at: \(Date())")
+            do {
+                try await Amplify.DataStore.save(todo)
+            } catch {
+                print("Error saving todo: \(error)")
+            }
+        }
     }
 }
+
+extension Todo: Identifiable { }
+
